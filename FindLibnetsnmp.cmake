17feeecc1491bcfc2097a930f94737f54e67e000
gureedo <gureedo@intersvyaz.net>
2015-04-02 16:05:27 +0500

    find net-snmp module-

---
+++ FindLibnetsnmp.cmake
@@ -0,0 +1,50 @@
+# - Find libnetsnmp
+# Find the native libnetsnmp includes and library.
+# Once done this will define
+#
+#  LIBNETSNNMP_INCLUDE_DIRS - where to find net-snmp/net-snmp-config.h, etc.
+#  LIBNETSNNMP_LIBRARIES    - List of libraries when using libnetsnp.
+#  LIBNETSNNMP_FOUND        - True if libnetsnmp found.
+
+FIND_PATH(LIBNETSNMP_INCLUDE_DIR NAMES net-snmp/net-snmp-config.h)
+
+FIND_LIBRARY(LIBNETSNMP_LIBRARY NAMES snmp netsnmp)
+
+MARK_AS_ADVANCED(LIBNETSNMP_LIBRARY LIBNETSNMP_INCLUDE_DIR)
+
+IF(LIBNETSNMP_INCLUDE_DIR AND EXISTS "${LIBNETSNMP_INCLUDE_DIR}/net-snmp/net-snmp-config.h")
+    # Read and parse net-snmp version header file for version number
+    FILE(READ "${LIBNETSNMP_INCLUDE_DIR}/net-snmp/net-snmp-config.h" _libnetsnmp_HEADER_CONTENTS)
+    STRING(REGEX MATCH "PACKAGE_VERSION +\"[0-9\\.]+\"" _libnetsnmp_HEADER_CONTENTS "${_libnetsnmp_HEADER_CONTENTS}")
+
+    IF(_libnetsnmp_HEADER_CONTENTS EQUAL "")
+        SET(LIBNETSNMP_VERSION_MAJOR 0)
+        SET(LIBNETSNMP_VERSION_MINOR 0)
+        SET(LIBNETSNMP_VERSION_UPDATE 0)
+        SET(LIBNETSNMP_VERSION_BUILD 0)
+    ELSE()
+        STRING(REGEX REPLACE "PACKAGE_VERSION +\"([0-9]+).*" "\\1" LIBNETSNMP_VERSION_MAJOR "${_libnetsnmp_HEADER_CONTENTS}")
+        STRING(REGEX REPLACE "PACKAGE_VERSION +\"[0-9]+\\.([0-9]+).*" "\\1" LIBNETSNMP_VERSION_MINOR "${_libnetsnmp_HEADER_CONTENTS}")
+        STRING(REGEX REPLACE "PACKAGE_VERSION +\"[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" LIBNETSNMP_VERSION_UPDATE "${_libnetsnmp_HEADER_CONTENTS}")
+        IF(_libnetsnmp_HEADER_CONTENTS MATCHES "PACKAGE_VERSION +\"[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+.*")
+            STRING(REGEX REPLACE "PACKAGE_VERSION +\"[0-9]+\\.[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" LIBNETSNMP_VERSION_BUILD "${_libnetsnmp_HEADER_CONTENTS}")
+        ELSE()
+            SET(LIBNETSNMP_VERSION_BUILD 0)
+        ENDIF()
+    ENDIF()
+
+    SET(LIBNETSNMP_VERSION_STRING "${LIBNETSNMP_VERSION_MAJOR}.${LIBNETSNMP_VERSION_MINOR}.${LIBNETSNMP_VERSION_UPDATE}.${LIBNETSNMP_VERSION_BUILD}")
+ENDIF()
+
+# handle the QUIETLY and REQUIRED arguments and set LIBNETSNMP_FOUND to TRUE if
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Libnetsnmp
+    REQUIRED_VARS LIBNETSNMP_LIBRARY LIBNETSNMP_INCLUDE_DIR
+    VERSION_VAR LIBNETSNMP_VERSION_STRING
+)
+
+IF(LIBNETSNMP_FOUND)
+    SET(LIBNETSNMP_INCLUDE_DIRS ${LIBNETSNMP_INCLUDE_DIR})
+    SET(LIBNETSNMP_LIBRARIES ${LIBNETSNMP_LIBRARY})
+ENDIF()
